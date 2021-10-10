0a4e70e6d0a95944a02bd1e3fec8211c4e50a35e
Trisatya-Programming <trisatyadidiek86@gmail.com>
2021-10-10 14:09:32 +0700

    photo

--- App/Commands/TrisatyaProgramming.py
+++ App/Commands/TrisatyaProgramming.py
@@ -1,5 +1,8 @@
 data = {
+    'photo' : 'https://images.unsplash.com/photo-1472457897821-70d3819a0e24?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2069&q=80',
     'message-1' : '<b>WELCOME TO DOSCOM</b>\n<i>DOSCOM X HACKTOBERFEST</i>\nContributed by Trisatya-Programming',
     'course-1' : 'Free Course = https://www.dicoding.com/academies/120',
     'course-2' : 'Free Course = https://www.dicoding.com/academies/313',
+    'api-1' : 'Free API = https://api.quran.com/api/v4',
+    'api-2' : 'Free API = https://anime-facts-rest-api.herokuapp.com/api/v1',
 }
