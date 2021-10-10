db59f2ed2f573283e8b3c38e30e68397ebc74a62
maulanaaghnii <maulanaaghni.contact@gmail.com>
2021-10-10 21:55:48 +0700

    Menambah Course 2 | by. maulanaaghnii

--- App/Commands/maulanaaghnii.py
+++ App/Commands/maulanaaghnii.py
@@ -4,7 +4,10 @@ data = {
 	<i> DOSCOM X HACKTOBER </i>\n
 	by. TB Maulana Aghni.
 	''', 
-	'course' : 'https://www.udemy.com/course/fundamentals-of-iot-internet-of-things/?couponCode=3F7C4FA3B50F3519BABA',
+	'course-1' : 'https://www.udemy.com/course/fundamentals-of-iot-internet-of-things/?couponCode=3F7C4FA3B50F3519BABA',
+	'course-2' : 'https://www.udemy.com/course/the-learn-javascript-course/?couponCode=TRY10FREE102103',
 	'api' : 'https://www.census.gov/data/developers/data-sets.html',
 	'photo' : 'https://www.exasol.com/app/uploads/2020/06/women-in-tech.jpg',
-}
+	}
+
+	#thanks Hacktober Fest 2021 
