6372ebcfdbd31876eed1d236439a0c7fc9846fc9
unknown <dewasinar16@gmail.com>
2021-10-10 11:34:20 +0700

    menambahkan file command

---
+++ App/Commands/Dewasurya16.py
@@ -0,0 +1,3 @@
+data = {
+    'message-1' : '<b> semoga cewek yang aku sukai Mau sama aku </b>\n<i>DOSCOM X HACKTOBERFEST</i>\nContributed by Dewasurya16',
+}
--- Global.py
+++ Global.py
@@ -1,4 +1,5 @@
 from App.Commands import *
 
 data = {
+    'Dewasurya16' : Dewasurya16,
 }
