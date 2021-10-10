4b39487d68885aed3b5dc1422a34a0232e0027d2
Trisatya-Programming <trisatyadidiek86@gmail.com>
2021-10-10 12:54:12 +0700

    course

--- App/Commands/Trisatya-Programming.py
+++ App/Commands/Trisatya-Programming.py
@@ -1,3 +1,5 @@
 data = {
     'message-1' : '<b>WELCOME TO DOSCOM</b>\n<i>DOSCOM X HACKTOBERFEST</i>\nContributed by Trisatya-Programming',
+    'course-1' : 'Free Course = https://www.dicoding.com/academies/120',
+    'course-2' : 'Free Course = https://www.dicoding.com/academies/313',
 }
