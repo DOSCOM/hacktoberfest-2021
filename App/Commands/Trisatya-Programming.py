6488c448766f080c72d31048fecf67863cff0c64
heipann <73393996+ivannoard@users.noreply.github.com>
2021-10-10 17:26:04 +0700

    Update Trisatya-Programming.py

--- App/Commands/Trisatya-Programming.py
+++ App/Commands/Trisatya-Programming.py
@@ -2,9 +2,6 @@ from App.Commands import *
 
 data = {
     'Trisatya-Programming' : Trisatya-Programming,
-}
-
-data = {
     'message-1' : '<b>WELCOME TO DOSCOM</b>\n<i>DOSCOM X HACKTOBERFEST</i>\nContributed by Trisatya-Programming',
     'course-1' : 'Free Course = https://www.dicoding.com/academies/120',
     'course-2' : 'Free Course = https://www.dicoding.com/academies/313',
