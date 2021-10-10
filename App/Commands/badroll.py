bec9b1c32c1a140dd12ebd9193be998eace92aea
Badrul Akbar A M <badrl.xrpl2017smkrus@gmail.com>
2021-10-10 13:25:03 +0700

    adding api

--- App/Commands/badroll.py
+++ App/Commands/badroll.py
@@ -4,4 +4,5 @@ data = {
         <i>lorem ipsum dolor sit amet</i>
         <p>made with love by Badroll</p>
     """,
+    'api' : 'Free API = https://dev.farizdotid.com/api/daerahindonesia/provinsi\nhttps://api.first.org/data/v1/countries',
 }
