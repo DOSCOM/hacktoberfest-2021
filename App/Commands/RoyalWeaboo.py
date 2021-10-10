be5d5b26244bd8811bc224388d091d90c8b1fd3a
RoyalWeaboo <malikazizali86@gmail.com>
2021-10-10 15:37:05 +0700

    Update File Command Menambah Foto

--- App/Commands/RoyalWeaboo.py
+++ App/Commands/RoyalWeaboo.py
@@ -1,3 +1,4 @@
 data = {
 	'message-1' : '<b>Happy Contributing in Hacktober-2021</b>\n Nishimiya selalu dihati !\n Contributed By RoyalWeaboo',
+	'photo' : 'https://1.bp.blogspot.com/-qLYbe7iSWZI/XhtCtpuKvWI/AAAAAAAADjA/tBy5kLRoP0cGxFQFSq_eZ1x4coUOh0g5QCLcBGAsYHQ/s1600/Shouko%2BNishimiya%2B-%2B5.jpg'
 }
