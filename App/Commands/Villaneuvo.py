3c3ff5d59e9dbb824f3470b757e9b8fac7111818
Daffa Reza <111202012812@mhs.dinus.ac.id>
2021-10-10 19:00:40 +0800

    Menambahkan Free Course Villaneuvo

--- App/Commands/Villaneuvo.py
+++ App/Commands/Villaneuvo.py
@@ -1,8 +1,10 @@
 data = {
+    'photo' : 'https://www.meme-arsenal.com/memes/48e262a742edd94ff236ddc4f1671731.jpg'
     'message-1' : '''
     <b> Selamat Berkontribusi </b>
     <i> DOSCOM x HACKTOBERFEST </i>
     Contributed By Villaneuvo
     ''',
+    'course' : 'Free Course = 1.https://www.udemy.com/course/git-bash/\n2.https://www.udemy.com/course/command-line/'
     
 }
