4bf9e0731ba8883d9a58548497253a778a5da30a
Lilik Wahyu Nugroho <56556391+lilikwn@users.noreply.github.com>
2021-10-10 17:13:17 +0700

    Update lilikwn.py

--- App/Commands/lilikwn.py
+++ App/Commands/lilikwn.py
@@ -3,6 +3,6 @@ data = {
     <b>Selamat Welcome<b>
     <i>DOSCOM X HACKTOBERFEST</i>
     lilikwn''',
-    'course-1': 'https://www.udemy.com/course/linux-fundamentals-for-cyber-security-ethical-hacking-basic/?source=@udemyCoursesFreeTelegram&couponCode=CYBER-MONTH',
-    'course-2': 'https://www.udemy.com/course/javascript-basics-start-coding-in-5-minutes-2019/?source=@udemyCoursesFreeTelegram&couponCode=null'
+    'course-1': 'Free Course = https://www.udemy.com/course/linux-fundamentals-for-cyber-security-ethical-hacking-basic/?source=@udemyCoursesFreeTelegram&couponCode=CYBER-MONTH',
+    'course-2': 'Free Course =https://www.udemy.com/course/javascript-basics-start-coding-in-5-minutes-2019/?source=@udemyCoursesFreeTelegram&couponCode=null'
 }
