433e1c661b2ddf8054b7ce799d02d636f45f080b
safiraauliar <safiraauliarohmah@gmail.com>
2021-10-10 12:55:17 +0700

    menambahkan photo

--- App/Commands/safiraauliar.py
+++ App/Commands/safiraauliar.py
@@ -1,7 +1,9 @@
 data={
+    'photo':'https://pixabay.com/images/id-4443243/',
     'message-1':'''
     <b>Selamat Berkontribusi</b>
     <i> DOSCOM X HACKTOBERFEST 2021<i>
     Contributed By safiraauliar
     ''',
+ 
 }
