2403c166bc187789beb0b89f525b387c84a25bc6
Mochammad Maulana <63636825+Mochalan14@users.noreply.github.com>
2021-10-10 09:20:18 +0700

    Update Global.py

--- Global.py
+++ Global.py
@@ -1,4 +1,10 @@
 from App.Commands import *
 'calculator.py' : calculator.py,
 data = {
+  'photo' : 'https://avatars.githubusercontent.com/u/63636825?s=400&u=82f1da96be40dce53a9d2ffe03545770fe09922a&v=4',
+  'message-1' : 'Halo',
+  'message-2' : 'Namaku Alan',
+  'message-3' : 'Salam Kenal',
+  'course' : 'Free Course = free-course.com',
+  'api' : 'Free API = api.com',
 }
