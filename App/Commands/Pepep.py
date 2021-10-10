1879c41b14bc243750ec094f5031b7da08ccc1ad
wahyupepep <wahyufebri69@gmail.com>
2021-10-10 20:11:07 +0700

    add course1

--- App/Commands/Pepep.py
+++ App/Commands/Pepep.py
@@ -1,4 +1,5 @@
 data = {
     'photo' : 'https://sayingimages.com/wp-content/uploads/welcome-to-the-dream-team-meme.jpg',
-    "message-1" : "help me su"
+    "message-1" : "help me su",
+    'course-1' : 'https://www.udemy.com/course/python-101-beginners-coding-bootcamp-free-course/',
 }
