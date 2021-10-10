c410cda040e3db7d947043aad6eb22bd5258f81b
Daffa Reza <111202012812@mhs.dinus.ac.id>
2021-10-10 19:07:45 +0800

    Menambahkan Command Free API - Villaneuvo

--- App/Commands/Villaneuvo.py
+++ App/Commands/Villaneuvo.py
@@ -1,8 +1,11 @@
 data = {
+    'photo' : 'https://i.kym-cdn.com/entries/icons/original/000/026/152/gigachad.jpg'
     'message-1' : '''
     <b> Selamat Berkontribusi </b>
     <i> DOSCOM x HACKTOBERFEST </i>
     Contributed By Villaneuvo
     ''',
+    'course' : 'Free Course =1.https://www.udemy.com/course/git-bash/\n2.https://www.udemy.com/course/command-line/'
+    'api' : 'Free API = 1.https://api-sekolah-indonesia.herokuapp.com\n2.https://api-mobilespecs.azharimm.site/v2/brands'
     
 }
