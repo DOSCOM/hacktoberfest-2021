33886a30a005e92d84255b5ffd688acc620d590d
rama1277 <111202012849@mhs.dinus.ac.id>
2021-10-10 22:43:39 +0700

    Menambahkan file kontribus rama1277

---
+++ App/Commands/rama1277.py
@@ -0,0 +1,5 @@
+data = {
+    'photo ' : 'https://i.ytimg.com/vi/DkZ654P8h28/maxresdefault.jpghttps://i.ytimg.com/vi/DkZ654P8h28/maxresdefault.jpg',
+    'message-1' : '<b>Selamat datang</b>''<i>DOSCOM X HACKTOBERFEAT</i>',
+<b>Selamat Berkontibusi</b>   
+}
