5d365af26732df0dc382c0e49cc282cadb00cb6e
Enrico Zada <enricozada@icloud.com>
2021-10-10 16:19:07 +0700

    menghapus foto

--- App/Commands/enricozd.py
+++ App/Commands/enricozd.py
@@ -1,4 +1,3 @@
 data = {
-	'photo' : 'https://www.rover.com/blog/wp-content/uploads/2017/02/pug-2648774_1920-960x540.jpg'
 	'message-1' : '<b>Selamat Datang</b>\n<i>lagi kontribusi di doscom nich</i>\n By Enrico',
 }
