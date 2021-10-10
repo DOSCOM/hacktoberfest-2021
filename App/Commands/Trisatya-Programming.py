3786bfd152e1735fb27ed6f72938ebd77469b013
Trisatya-Programming <trisatyadidiek86@gmail.com>
2021-10-10 13:58:37 +0700

    api

--- App/Commands/Trisatya-Programming.py
+++ App/Commands/Trisatya-Programming.py
@@ -1,3 +1,13 @@
+from App.Commands import *
+
+data = {
+    'Trisatya-Programming' : Trisatya-Programming,
+}
+
 data = {
     'message-1' : '<b>WELCOME TO DOSCOM</b>\n<i>DOSCOM X HACKTOBERFEST</i>\nContributed by Trisatya-Programming',
+    'course-1' : 'Free Course = https://www.dicoding.com/academies/120',
+    'course-2' : 'Free Course = https://www.dicoding.com/academies/313',
+    'api-1' : 'Free API = https://api.quran.com/api/v4',
+    'api-2' : 'Free API = https://anime-facts-rest-api.herokuapp.com/api/v1',
 }
