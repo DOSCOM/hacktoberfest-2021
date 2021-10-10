6918196e4a136bdc75fecd1e91285f5cda0daf77
lilikwn <lilik1.wahyu7@gmail.com>
2021-10-10 18:46:05 +0700

    Menambahkan Photo | lilikwn

--- App/Commands/lilikwn.py
+++ App/Commands/lilikwn.py
@@ -1,4 +1,5 @@
 data = {
+    'photo': 'https://images.unsplash.com/photo-1521714161819-15534968fc5f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c3BpZGVybWFufGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
     'message-1': '''
     <b>Selamat Welcome<b>
     <i>DOSCOM X HACKTOBERFEST</i>
