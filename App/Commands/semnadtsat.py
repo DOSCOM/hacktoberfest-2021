2bbc57ed9c0be82794427d8e6de5010e35d3a3cb
semnadtsat <88280973+semnadtsat@users.noreply.github.com>
2021-10-11 21:50:54 +0700

    Menambah API

--- App/Commands/semnadtsat.py
+++ App/Commands/semnadtsat.py
@@ -1,6 +1,9 @@
 data = {
+    'photo' : 'https://images6.alphacoders.com/851/851558.png',
     'message-1' : '''
     <b>Don't Panic</b>
     <i>DOSCOM X HACKTOBERFEST</i>
-    Contributed by Semnadtsat'''
+    Contributed by Semnadtsat''',
+    'course' : 'Free Course = https://www.udemy.com/course/belajar-restful-api-untuk-pemula/',
+    'api' : 'Free API = https://api2.binance.com/api/v3/ticker/24hr',
 }
