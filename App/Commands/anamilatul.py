4fce191292a6e62fff3c72aa1e43df75e444af4c
anamilatul <anamilatulaulia@gmail.com>
2021-10-11 19:15:17 +0700

    Menambahkan course | anamilatul

--- App/Commands/anamilatul.py
+++ App/Commands/anamilatul.py
@@ -1,3 +1,5 @@
 data = {
     'message-1' : '<b>Selamat Berkontribusi</b> <i>Doscom X Hacktoberfest</i> Contributed By Ana Mila',
+    'course-1' : 'Free Course  = https://www.udemy.com/course/build-a-quiz-app-with-html-css-and-javascript/',
+    'course-2' : 'Free Course = https://www.udemy.com/course/how-to-build-and-make-a-wordpress-website-from-scratch-2017/',
 }
