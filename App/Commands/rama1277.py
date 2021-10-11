a5dede403a35f2e23824d7129d88135345600e66
rama1277 <111202012849@mhs.dinus.ac.id>
2021-10-11 08:14:15 +0700

    Menambahkan file kolab rama1277

--- App/Commands/rama1277.py
+++ App/Commands/rama1277.py
@@ -1,5 +1,8 @@
 data = {
     'photo ' : 'https://i.ytimg.com/vi/DkZ654P8h28/maxresdefault.jpghttps://i.ytimg.com/vi/DkZ654P8h28/maxresdefault.jpg',
     'message-1' : '<b>Selamat datang</b>''<i>DOSCOM X HACKTOBERFEAT</i>',
-<b>Selamat Berkontibusi</b>   
+    '''''
+<b>Selamat Berkontibusi</b>  
+<i>DOSCOM X HACKTOBERFAST kolaborasi</i>
+    '''' 
 }
