2d152346410f80d343bae0d6b4ed0258688b7b98
maulanaaghnii <maulanaaghni.contact@gmail.com>
2021-10-11 21:19:24 +0700

    Menambah API

--- App/Commands/maulanaaghnii.py
+++ App/Commands/maulanaaghnii.py
@@ -4,4 +4,6 @@ data = {
 	<i> DOSCOM X HACKTOBER </i>\n
 	by. TB Maulana Aghni.
 	''', 
+        'api' = 'https://github.com/robvanbakel/gotiny-api',
+        'photo' = 'https://media.istockphoto.com/photos/futuristic-abstract-background-picture-id1253467286?b=1&k=20&m=1253467286&s=170667a&w=0&h=ZPT2DwfAVvoSXXTzK9Zix1sNUIRLDFEfsEUx0NsLCX8=',
 }
