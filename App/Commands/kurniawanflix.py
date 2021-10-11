460ff7b68407a613c7defc85593d94ce0e4c3d44
Muhammad David Kurniawan <kurniawanflix@gmail.com>
2021-10-11 19:30:25 +0700

    Closed #241 Mengganti Foto

--- App/Commands/kurniawanflix.py
+++ App/Commands/kurniawanflix.py
@@ -1,6 +1,9 @@
 data = {
-  'message-1' : '''
+    'message-1' : '''
     <b> Selamat Berkontribusi </b>
     <i> DOSCOM x HACKTOBERFEST </i>
     ''',
+    
+    'photo' : 'https://images.wallpaperscraft.com/image/single/girl_shell_hare_167320_300x168.jpg',
+    
 }
