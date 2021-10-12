92fa537e49a822cac76ebd24d63d6b7fef9c5c4e
atDnK <ditakusuma66@gmail.com>
2021-10-12 11:11:16 +0700

    Menambahkan api

--- App/Commands/dinitaks.py
+++ App/Commands/dinitaks.py
@@ -5,4 +5,5 @@ data = {
     Dinita Has Contributed''',
     'photo' : 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.resepkekinian.com%2Frecipe%2Fternyata-begini-cara-bikin-roti-tawar-lembut-membal-bak-squishy%2F&psig=AOvVaw0q2V2gwW6vfIfxNNgc5h7X&ust=1634095692743000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCMDcuIH3w_MCFQAAAAAdAAAAABAD',
     'course' : 'https://www.udemy.com/course/web-design-for-beginners-real-world-coding-in-html-css/',
+    'api' : 'https://www.mediawiki.org/wiki/API:Tutorial',
 }
