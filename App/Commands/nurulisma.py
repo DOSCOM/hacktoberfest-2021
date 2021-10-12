5d738c45d83e72ddaa9d25f8d2942cac55e59ae4
Nurul-Isma <nurulismawati236@gmail.com>
2021-10-12 10:15:22 +0700

    issue-265

--- App/Commands/nurulisma.py
+++ App/Commands/nurulisma.py
@@ -1,4 +1,5 @@
 data = {
+    'photo' : "https://images.unsplash.com/photo-1633531138911-1dffbaa8d65d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=391&q=80",
     'message-1' : '''
     <b>DOSCOM x HACKTOBERFEST2021</b>
     Nurul Has Contributed''',
