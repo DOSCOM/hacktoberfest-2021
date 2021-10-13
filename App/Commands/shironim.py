e215f185e26614afdf4c00a10199cc0bdec8f462
Shironim <dimasseto.id@gmail.com>
2021-10-13 17:03:54 +0700

    add photo 2 | shironim

--- App/Commands/shironim.py
+++ App/Commands/shironim.py
@@ -1,4 +1,4 @@
 data = {
-    'message-1': '<b>Hai, Aku Shironim</b> \n <i>DOSCOM X HACKTOBERFEST</i> \n By Dimas Seto',
+    'message-1': '<b>Halo, Aku Shironim</b> \n <i>DOSCOM X HACKTOBERFEST</i> \n By Shironim',
     'photo': 'https://avatars.githubusercontent.com/u/31839535?v=4',
 }
