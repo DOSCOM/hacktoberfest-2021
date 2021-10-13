bf2debb50da48772365492b699c7cfcee673fe48
unknown <dewasinar16@gmail.com>
2021-10-13 17:49:59 +0700

    anjir salah poto istri

--- App/Commands/Dewasurya16.py
+++ App/Commands/Dewasurya16.py
@@ -1,5 +1,5 @@
 data = {
-      'photo' : '<https://www.google.com/url?sa=i&url=https%3A%2F%2Fdailyspin.id%2Fother-games%2Fgenshin-impact%2Fkelebihan-dan-kelemahan-keqing-genshin-impact%2F&psig=AOvVaw0zLn37i7MPuHysFts24bOn&ust=1634205885953000&source=images&cd=vfe&ved=0CAkQjRxqFwoTCKCP_7qRx_MCFQAAAAAdAAAAABAJ>',
-      'photo' : '<https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.vg247.com%2Fgenshin-impact-kokomi-build&psig=AOvVaw2DeyThv7nGGqc92uEMP1Pf&ust=1634207093156000&source=images&cd=vfe&ved=0CAkQjRxqFwoTCODGwfuVx_MCFQAAAAAdAAAAABAD>',
+      'photo' : '<https://images.app.goo.gl/c42fskcR7d9SGucLA>',
+      'photo' : '<https://images.app.goo.gl/zQXZQNkX8HFoYk2j6>',
     'message-1' : '<b> semoga cewek yang aku sukai Mau sama aku </b>\n<i>DOSCOM X HACKTOBERFEST</i>\nContributed by Dewasurya16',
 }
