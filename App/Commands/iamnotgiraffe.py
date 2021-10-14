98c384b693e05d61dcd1a1855eca4ed717f7f9f9
iamnotgiraffe <asuriyabs@gmail.com>
2021-10-14 12:40:24 +0700

    add photo

--- App/Commands/iamnotgiraffe.py
+++ App/Commands/iamnotgiraffe.py
@@ -1,4 +1,5 @@
 data = {
 'message-1' : 'Halo Selamat Datang, Aku Bruang',
 'message-2' : 'Salam Kenal Semuanya, hehehe',
+'photo' : 'https://assets.vogue.com/photos/614cdedaa0ced20e09d4e63c/master/w_1600,c_limit/OP21108_AM_US_Vogue_5161_09_sRGB.jpg',
 }
