8e6755705ee654bf343efabcf50794b875e9a050
derynm <nugrohodery682@gmail.com>
2021-10-14 17:00:00 +0700

    add photo

--- App/Commands/derynm.py
+++ App/Commands/derynm.py
@@ -1,4 +1,5 @@
 data = {
+    'photo' : 'https://drive.google.com/file/d/1d7vlD81EnA1AiJGkFERyLTjmnaG7k0aN/view?usp=sharing',
     'message-1' : '''
     <b>Halo</b>
     <i>DOSCOM x HACKTOBERFEST</i>
