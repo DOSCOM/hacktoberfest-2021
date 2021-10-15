74ade31b2453d1bb7ee76ea2b35cc9ac9be3b35e
iamnotgiraffe <asuriyabs@gmail.com>
2021-10-15 14:45:13 +0700

    add api

--- App/Commands/iamnotgiraffe.py
+++ App/Commands/iamnotgiraffe.py
@@ -2,4 +2,5 @@ data = {
 'message-1' : 'Halo Selamat Datang, Aku Bruang',
 'message-2' : 'Salam Kenal Semuanya, hehehe',
 'photo' : 'https://assets.vogue.com/photos/614cdedaa0ced20e09d4e63c/master/w_1600,c_limit/OP21108_AM_US_Vogue_5161_09_sRGB.jpg',
+'api' : 'https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/UK/GBP/en-GB/',
 }
