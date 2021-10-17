6db90820f219a90b792b6e469b3f14581c5fcc01
husin14 <husinsmg@gmail.com>
2021-10-17 13:39:11 +0700

    arti TM untuk semarang

--- App/Commands/husin14.py
+++ App/Commands/husin14.py
@@ -2,5 +2,8 @@ data = {
     'message-1' : 'Kota Semarang adalah ibukota Provinsi Jawa Tengah, Indonesia sekaligus kota metropolitan terbesar kelima di Indonesia',
     
     'photo' : 'https://www.goodnewsfromindonesia.id/uploads/post/large-tugu-muda-lawang-sewu-kota-semarang-2019-sam9042-2aee8bdfe52106aa5ca1b46aafe2c3db.jpg',
-    'message-2' : 'foto diatas adalah foto tugu muda semarang,. ini deket udinus kok bos. jalan kaki aja'
+    'message-2' : 'foto diatas adalah foto tugu muda semarang,. ini deket udinus kok bos. jalan kaki aja',
+    'message-2' : 'Tugu Muda ini menggambarkan tentang semangat berjuang & patriotisme warga semarang, khususnya para pemuda yang gigih, rela berkorban dengan semangat yang tinggi mempertahankan Kemerdekaan Indonesia.'
+
+
 }
