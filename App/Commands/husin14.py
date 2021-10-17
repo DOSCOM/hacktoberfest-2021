f713785ec2cfa441d3f8dafb13827595ac11ba2f
husin14 <husinsmg@gmail.com>
2021-10-17 13:28:51 +0700

    desc TM smg

--- App/Commands/husin14.py
+++ App/Commands/husin14.py
@@ -1,6 +1,6 @@
 data = {
     'message-1' : 'Kota Semarang adalah ibukota Provinsi Jawa Tengah, Indonesia sekaligus kota metropolitan terbesar kelima di Indonesia',
     
-    'photo' : 'https://www.goodnewsfromindonesia.id/uploads/post/large-tugu-muda-lawang-sewu-kota-semarang-2019-sam9042-2aee8bdfe52106aa5ca1b46aafe2c3db.jpg'
-
+    'photo' : 'https://www.goodnewsfromindonesia.id/uploads/post/large-tugu-muda-lawang-sewu-kota-semarang-2019-sam9042-2aee8bdfe52106aa5ca1b46aafe2c3db.jpg',
+    'message-2' : 'foto diatas adalah foto tugu muda semarang,. ini deket udinus kok bos. jalan kaki aja'
 }
