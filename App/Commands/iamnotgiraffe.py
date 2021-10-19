eebfe2840d453d508329399575ab4616e30173ff
iamnotgiraffe <asuriyabs@gmail.com>
2021-10-19 08:48:42 +0700

    penambahan pesan 3

--- App/Commands/iamnotgiraffe.py
+++ App/Commands/iamnotgiraffe.py
@@ -3,4 +3,5 @@ data = {
 'message-2' : 'Salam Kenal Semuanya, hehehe',
 'photo' : 'https://assets.vogue.com/photos/614cdedaa0ced20e09d4e63c/master/w_1600,c_limit/OP21108_AM_US_Vogue_5161_09_sRGB.jpg',
 'api' : 'https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/UK/GBP/en-GB/',
+'message-3' : 'Terima Kasih, sampai jumpa',
 }
