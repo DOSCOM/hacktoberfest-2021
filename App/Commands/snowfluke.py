3386fc80eea036a3a6bdc06ad295c5dea84788b7
Awal Ariansyah <57579406+snowfluke@users.noreply.github.com>
2021-10-20 07:58:02 +0700

    Create snowfluke.py

---
+++ App/Commands/snowfluke.py
@@ -0,0 +1,6 @@
+data = {
+  'photo' : 'https://avatars.githubusercontent.com/u/57579406?v=4',
+  'message-1' : 'Semoga harimu menyenangkan',
+  'course' : 'Free Course Node.js Beginner = https://www.udemy.com/course/node-js-api-tutorial/',
+  'api' : 'Free API Quotes = https://goquotes.docs.apiary.io/',
+}
