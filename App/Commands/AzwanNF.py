0c91b910baf5e15144b683d460d07d79fd2b9af8
AzwanNF <fadhil.insiderzz@gmail.com>
2021-10-27 14:29:15 +0700

    Menambah Command 3

--- App/Commands/AzwanNF.py
+++ App/Commands/AzwanNF.py
@@ -3,6 +3,7 @@ data = {
     'message-1': '<b>Gitar ku petik, bass ku betot',
     'message-2': '<b>Hai nona cantik, bass kubetot</b>',
     'message-3': '<b>saya sih yes</b>',
+    'message-4': '<b>saya wibu</b>',
     'course': 'learning-how-to-learn = https://www.coursera.org/learn/learning-how-to-learn',
     'api': 'Custom search : https://any-api.com/googleapis_com/customsearch/docs/API_Description',
 }
