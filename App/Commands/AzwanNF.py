7249c2ce933098dde61da8ae42e8cd695d908c0b
AzwanNF <fadhil.insiderzz@gmail.com>
2021-10-27 14:14:16 +0700

    Menambah AzwanNF.py

--- App/Commands/AzwanNF.py
+++ App/Commands/AzwanNF.py
@@ -2,4 +2,7 @@ data = {
     'photo': 'https://drive.google.com/file/d/1wzJA7CyS4JKbiYO6Z76M0w4oSGQQrJHi/view?usp=sharing',
     'message-1': '<b>Gitar ku petik, bass ku betot',
     'message-2': '<b>Hai nona cantik, bass kubetot</b>',
+    'message-3': '<b>saya sih yes</b>',
+    'course': 'learning-how-to-learn = https://www.coursera.org/learn/learning-how-to-learn',
+    'api': 'Custom search : https://any-api.com/googleapis_com/customsearch/docs/API_Description',
 }
