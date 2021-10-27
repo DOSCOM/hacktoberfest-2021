a7fcda7886c102544a71515cdcc9de2fe6c08f3b
AzwanNF <fadhil.insiderzz@gmail.com>
2021-10-27 15:33:35 +0700

    nambahin comment 5

--- App/Commands/AzwanNF.py
+++ App/Commands/AzwanNF.py
@@ -4,6 +4,7 @@ data = {
     'message-2': '<b>Hai nona cantik, bass kubetot</b>',
     'message-3': '<b>saya sih yes</b>',
     'message-4': '<b>Saya Wibu tapi gak akuts</b>',
+    'message-5': '<b>saya dah capek</b>',
     'course': 'learning-how-to-learn = https://www.coursera.org/learn/learning-how-to-learn',
     'api': 'Custom search : https://any-api.com/googleapis_com/customsearch/docs/API_Description',
 }
