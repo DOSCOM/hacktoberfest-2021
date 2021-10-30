8eadd35dcec969a8ea4f03413410fc523df8c02b
unknown <dewasinar16@gmail.com>
2021-10-30 12:19:23 +0700

    menambah text

--- App/Commands/Dewasurya16.py
+++ App/Commands/Dewasurya16.py
@@ -2,4 +2,5 @@ data = {
       'photo' : '<https://images.app.goo.gl/c42fskcR7d9SGucLA>',
       'photo' : '<https://images.app.goo.gl/zQXZQNkX8HFoYk2j6>',
     'message-1' : '<b> semoga cewek yang aku sukai Mau sama aku </b>\n<i>DOSCOM X HACKTOBERFEST</i>\nContributed by Dewasurya16',
+    'message-2' : '<b> Bismillah dapat hatinya </b>\n<i>DOSCOM X HACKTOBERFEST</i>\nContributed by Dewasurya16',
 }
