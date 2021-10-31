32421d4c0b192864ecccc2cbd6fe8b9e006ecf51
= <=>
2021-10-31 13:26:25 +0700

    Menambahkan pesan

--- App/Commands/novanauliazami.py
+++ App/Commands/novanauliazami.py
@@ -1,4 +1,6 @@
 data = {
-   'photo':  'https://avatars.githubusercontent.com/u/44310374?v=4' ,
-   'message-1': 'Halo semua.'
+   'photo':  'https://avatars.githubusercontent.com/u/44310374?v=4',
+   'message-1': 'Halo semua.',
+   'message-2': 'Bagaimana kabarnya?',
+   'message-3': 'Semoga dalam keadaan baik-baik saja, ya.'
 }
