90b9087762ec6bea1c6b188a8024e514efa74f3a
= <=>
2021-10-31 14:06:53 +0700

    Add Free API

--- App/Commands/novanauliazami.py
+++ App/Commands/novanauliazami.py
@@ -3,5 +3,6 @@ data = {
    'message-1': 'Halo semua.',
    'message-2': 'Bagaimana kabarnya?',
    'message-3': 'Semoga dalam keadaan baik-baik saja, ya.',
-   'course': 'Free Course = http://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-006-introduction-to-algorithms-fall-2011/lecture-videos/'
+   'course': 'Free Course = http://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-006-introduction-to-algorithms-fall-2011/lecture-videos/',
+   'api': 'Free API = https://kawalcorona.com/api/'
 }
